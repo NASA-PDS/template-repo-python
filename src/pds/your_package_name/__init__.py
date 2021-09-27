f2460e5b785847b032028108fec02afb03589454
Michael Joyce <Michael.J.Joyce@jpl.nasa.gov>
2021-09-27 14:10:02 -0700

    Remove versioneer and cleanup package handling
    Drop versioneer from the template repository due to a litany of issues
    with versioneer and our builds. Ultimately, the small amount of
    effort saved around version number handling was far out weighed by the
    frequent issues and messy workarounds needed to integrate it. Version
    handling has been moved to `pds/your_package_name/__init__.py` where
    `__version__` is defined.
    
    A few package updates were possible / necessary after dropping
    versioneer. We can now use the `build-backend` option `in pyproject.toml`.
    The `setup.py` file is kept around only to facilitate cmd_class
    additions such as `build_sphinx`. We should explore removing that.
    Additionally, now we properly search for namespace packages in `setup.cfg`.
    
    The `tox` configuration has been updated to use "isolated builds". AKA,
    tell `tox` to use the `build-system` defined in `pyproject.toml`. At the
    end of the day, this doesn't really change much of anythign since we're
    using `setuptools`.
    
    Resolve #50

--- src/pds/your_package_name/__init__.py
+++ src/pds/your_package_name/__init__.py
@@ -1,10 +1,7 @@
 # -*- coding: utf-8 -*-
 """My PDS Module."""
-from ._version import get_versions
 
-__version__ = get_versions()["version"]
-__date__ = get_versions()["date"]
-del get_versions
+__version__ = "0.1.0"
 
 
 # For future consideration:
