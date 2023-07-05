defb0be6052b93be70d344030aba315b8e3383e4
Sagar Joshi <sagar.joshi@jpl.nasa.gov>
2023-07-05 13:56:09 -0700

    new tf workflow

---
+++ terraform/variables.tf
@@ -0,0 +1,11 @@
+variable "ami" {
+  type        = string
+  description = "example ami id"
+  default     = "ami-830c94e3"
+}
+
+variable "instance_type" {
+  type        = string
+  description = "example instance type"
+  default     = "t2.micro"
+}
